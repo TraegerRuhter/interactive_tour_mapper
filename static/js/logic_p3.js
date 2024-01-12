d346b7ee99a35370913ac32e970299363c7cede4
matin-fae <matin.faith@gmail.com>
2024-01-11 21:13:55 -0800

    map up and running

---
+++ static/js/logic_p3.js
@@ -0,0 +1,11 @@
+var map = L.map('map').setView([44.0448, -123.0726], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+
+
+
+
